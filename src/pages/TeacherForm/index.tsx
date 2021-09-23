2612d1aa07e4a7c193bead269a85df5ca5e58237
Adriano Castro <eaadrianosilvacastro@unipam.edu.br>
2021-09-22 21:07:23 -0300

    Add novo horario

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -15,6 +15,17 @@ import './styles.css';
 
 
 function TeacherForm() {
+      const [scheduleItems, setScheduleItems] = useState([
+            { week_day: 0, from: '', to: '' }
+      ]);
+
+      function addNewScheduleItem() {
+            setScheduleItems([
+                  ...scheduleItems,
+                  { week_day: 0, from: '', to: '' }
+            ]);
+      }
+
       return (
             <div id="page-teacher-form" className="container">
                   <PageHeader
@@ -52,31 +63,32 @@ function TeacherForm() {
                         <fieldset>
                               <legend>
                                     Horários disponíveis
-                                    <button type="button">
+                                    <button type="button" onClick={addNewScheduleItem}>
                                           + Novo horário
                                     </button>
                               </legend>
+                              {scheduleItems.map(scheduleItem => {
+                                    return (
+                                          <div key={scheduleItem.week_day} className="schedule-item">
+                                                <Select
+                                                      name="week_day"
+                                                      label="Dia da semana"
+                                                      options={[
+                                                            { value: '0', label: 'Domingo' },
+                                                            { value: '1', label: 'Segunda-feira' },
+                                                            { value: '2', label: 'Terça-feira' },
+                                                            { value: '3', label: 'Quarta-feira' },
+                                                            { value: '4', label: 'Quinta-feira' },
+                                                            { value: '5', label: 'Sexta-feira' },
+                                                            { value: '6', label: 'Sábado' },
+                                                      ]}
+                                                />
+                                                <Input name="from" label="Das" type="time" />
+                                                <Input name="to" label="Até" type="time" />
+                                          </div>
+                                    );
+                              })}
                         </fieldset>
-
-                        <div className="schedule-item">
-                        <Select
-                                    name="subject"
-                                    label="Matéria"
-                                    options={[
-                                          { value: 'Artes', label: 'Artes' },
-                                          { value: 'Biologia', label: 'Biologia' },
-                                          { value: 'Geoagrafia', label: 'Geoagrafia' },
-                                          { value: 'Literatura', label: 'Literatura' },
-                                          { value: 'Matematica', label: 'Matematica' },
-                                          { value: 'Inglês', label: 'Inglês' },
-                                          { value: 'Programação', label: 'Programação' },
-                                    ]}
-                              />
-                              <Input name="from" label = "Das" type = "time" />
-                              <Input name="from" label = "Das" type = "time" />
-                              
-                        </div>
-
                         <footer>
                               <p>
                                     <img src={warningIcon} alt="IMPORTANTE" />
